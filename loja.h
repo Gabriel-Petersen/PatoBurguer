857777a8f73698fa50ee39bb2983ccc17064462d
DESKTOP-8K6TDRD\Mateus PC <mateussburgos@gmail.com>
2025-10-30 21:49:32 -0300

    Lógica da loja finalizada

--- loja.h
+++ loja.h
@@ -1,13 +1,12 @@
+#ifndef LOJA_H
+#define LOJA_H
+#define USE_SHORTCUTS
 #include"include/graphycs_all.h"
-#include"itens/cardapio.h"
-#include"estruturas_de_dados/fila_dinamica.h"
 #include"estruturas_de_dados/listade.h"
-#include"estruturas_de_dados/pilha.h"
 #include"assets/piskel_ingredientes.h"
-#include"assets/mcBackground.h"
 #include"assets/piskel_molhos.h"
 #include"jogador.h"
-void mostrar_controles(Screen* atual){
+void mostrar_controles(){
     printf("Controles:\n");
     printf("X: Sair da Loja | ");
     printf("D: Próximo Ingrediente | ");
@@ -16,8 +15,8 @@ void mostrar_controles(Screen* atual){
     printf("V: Vender Ingrediente | ");
     return;
 }
-void mostrar_grana(Jogador *J){
-	printf("Quantidade de moedas: %Lf",J.dinheiro);
+inline void mostrar_grana(Jogador *j){
+	printf("\nQuantidade de moedas: %.2Lf",j->dinheiro);
 	return;
 }
 void deixa_circular(Lista_DE *l){
@@ -33,67 +32,79 @@ void preenche_ingredientes(Ingrediente ingredientes[]){
     ingredientes[4]=QUEIJO;
     ingredientes[5]=ALFACE;
     ingredientes[6]=TOMATE;
-    ingredientes[7]=MAIONESE_TEMPERADA;
-    ingredientes[8]=BARBERCUE;
-    ingredientes[9]=CEBOLA_CARAMELIZADA;
-    ingredientes[10]=PICLES;
-    ingredientes[11]=OVO_FRITO;
-    ingredientes[12]=ONION_RINGS;
+    ingredientes[7]=CEBOLA_CARAMELIZADA;
+    ingredientes[8]=PICLES;
+    ingredientes[9]=OVO_FRITO;
+    ingredientes[10]=ONION_RINGS;
+	ingredientes[11]=MAIONESE_TEMPERADA;
+    ingredientes[12]=BARBERCUE;
 	return;
 }
 typedef struct{
 	Screen *main_tela;
-    Screen *cd_tela;
-    Obj fundo_loja;
-}CenarioLoja
+    Obj fundo_loja; // Se sobrar tempo, fazer um desenho no piskel e colocar o arquivo na pasta assets para fazer um fundinho p/ loja
+}CenarioLoja;
 void iniciar_loja(Jogador *J){
-	CenarioLoja Cenario;
-	Cenario.main_tela=criar_tela(nv2(131,31),COLOR_CIANO,50);
-	tela=Cenario.main_tela;
+	CenarioLoja cenario;
+	cenario.main_tela=criar_tela(nv2(131,31),COLOR_CIANO,50);
+	//cenario.fundo_loja=criar_piskel_obj();
+	centralizar_objeto(cenario.fundo_loja);
 	printf("Bem vindo à loja!\n");
 	printf("Aqui você pode comprar e vender ingredientes. Fique atento à sua quantidade de moedas, pois se elas acabarem você perde o jogo.\n");
 	printf("Boas compras!\n");
+	printf("Aperte qualquer tecla para continuar\n");
+	getchar();
 	Lista_DE *I=inicializa_lista();
 	Ingrediente ingredientes[QTD_INGREDIENTES];
 	preenche_ingredientes(ingredientes);
 	for(int i=0;i<QTD_INGREDIENTES;i++){
 		ItemDaLista Item;
 		Item.ing=ingredientes[i];
-		Item.obg=criar_piskel_obj(ingredientes_data[i],INGREDIENTES_FRAME_WIDTH,INGREDIENTES_FRAME_HEIGHT);
+		if(i<11)Item.obj=criar_piskel_obj(ingredientes_data[i],INGREDIENTES_FRAME_WIDTH,INGREDIENTES_FRAME_HEIGHT);
+		else Item.obj=criar_piskel_obj(molhos_data[i-11],MOLHOS_FRAME_WIDTH,MOLHOS_FRAME_HEIGHT);
 		insere_fim(I,Item);
-		centralizar_objeto(Item.obg);
+		centralizar_objeto(Item.obj);
 	}
 	deixa_circular(I);
 	Nodo *p=I->ini;
+	desenhar_objeto(cenario.main_tela,p->info.obj);
 	while(true){
-		render(tela,true);
+		render(cenario.main_tela,true);
 		mostrar_controles();
 		char input=ler_teclado();
 		switch(input){
 			case 'X':
+				esconder_objeto(cenario.main_tela,p->info.obj);
+				for(int i=0,Nodo *atu=I->ini;i<QTD_INGREDIENTES;i++,atu=atu->prox)excluir_objeto(atu->info.obj);
+				destruir_lista(I);
+				//esconder_objeto(cenario.main_tela,cenario.fundo_loja);
+				//excluir_objeto(cenario.fundo_loja);
+				excluir_tela(cenario.main_tela);
 				return;
 			case 'D':
-				esconder_objeto(tela,p->obj);
+				esconder_objeto(cenario.main_tela,p->info.obj);
 				p=p->prox;
-				desenhar_objeto(tela,p->obj);
+				desenhar_objeto(cenario.main_tela,p->info.obj);
 				break;
 			case 'A':
-				esconder_objeto(tela,p->obj);
+				esconder_objeto(cenario.main_tela,p->info.obj);
 				p=p->ant;
-				desenhar_objeto(tela,p->obj);
+				desenhar_objeto(cenario.main_tela,p->info.obj);
 				break;
 			case 'C':
-				if(J->dinheiro>=p->ing->valor){
-					J->dinheiro-=p->ing->valor;
+				if(J->dinheiro>=p->info.ing.valor){
+					J->dinheiro-=p->info.ing.valor;
 					//adiciona o ingrediente ao estoque
 				}
 				else printf("Dinheiro insuficiente\n");
 				break;
 			case 'V':
-				J->dinheiro+=p->ing->valor;
+				//if(buscar_no_estoque(jogador->estoque,p->info.ing)==NULL)break;
+				J->dinheiro+=p->info.ing.valor;
 				//remove o ingrediente do estoque
 				break;
 		}
 		mostrar_grana(J);
 	}
 }
+#endif
